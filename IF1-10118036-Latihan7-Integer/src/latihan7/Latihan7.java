b436aa4b5b072e68a17946f04dd68348d28c1673
Adrian C. Senna <55816324+Raykitsuu@users.noreply.github.com>
2019-09-26 19:26:50 +0700

    Add files via upload

---
+++ IF1-10118036-Latihan7-Integer/src/latihan7/Latihan7.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan7;
+
+/**
+ *
+ * @author
+ * NAMA			: Adrian C. Senna
+ * KELAS		: IF-1
+ * NIM			: 10118036
+ * Deskripsi Program	: Program ini berisi program untuk memperlihatkan variable integer.
+ */
+public class Latihan7 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		byte b;
+		short s;
+		int i;
+		long l;
+		b = 120;
+		s = 32767;
+		i = 1_000_000_000;
+		l = 9000000000L;
+		System.out.println("Bute : " + b);
+		System.out.println("Short : " + s);
+		System.out.println("int : " + i);
+		System.out.println("long : " + l);
+		// TODO code application logic here
+	}
+	
+}
